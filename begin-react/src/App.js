c39dfc81f6708d251d4cc1c466bc748a522e363c
han-na-yang <hn.yang@apexesc.com>
2025-08-13 18:13:05 +0900

    [react-begin] ~18강
    Hook 확인

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import { useRef , useState } from "react";
+import { useRef , useState , useMemo , useCallback} from "react";
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
@@ -10,6 +10,18 @@ import UserListArray from "./UserListArray";
 import UserListArrayUseRef from "./UserListArrayUseRef";
 import CreateUser from "./CreateUser";
 
+function countActiveUsers(users) {
+	console.log("활성 사용자 수를 세는중...");
+	return users.filter(user => user.active).length;
+}
+/*
+	리렌더링 될때마다 함수가 실행 되는데
+	현재 App 에서는 onChange 이벤트를 발생 시켜서 상태를 변경 할 때에도
+	화면은 계속 리렌더링 됨.
+	불필요하게 함수가 계속 실행됨.
+	이럴때 사용할 수 있는게 useMemo (특정 값이 바뀌었을 때만 특정 함수 실행해서 연산하도록 실행)
+*/
+
 function App() {
 
 	const [inputs, setInputs] = useState({
@@ -18,36 +30,39 @@ function App() {
 	})
 
 	const {username, email} = inputs;
-	const onChange = e => {
+	const onChange = useCallback(e => {
 		const {name, value} = e.target;
 		setInputs({
 			...inputs,
 			[name]: value
 		});
-	};
+	}, [inputs]);	// inputs 가 바뀔때만 함수가 새로 만들어지고 그렇지 않다면 만들어져있는 함수가 실행
 
 	const [users, setUsers] = useState([
 		{
 			id:1,
 			username:'hanna1',
-			email:'test1@gmail.com'
+			email:'test1@gmail.com',
+			active: true,
 		},
 		{
 			id:2,
 			username:'hanna2',
-			email:'test2@gmail.com'
+			email:'test2@gmail.com',
+			active: false,
 		},
 		{
 			id:3,
 			username:'hanna3',
-			email:'test3@gmail.com'
+			email:'test3@gmail.com',
+			active: false,
 		},
 	]);
 
 	const nextId = useRef(4);
 	// useRef 를 사용하는 이유는 해당 값이 변경 될때마다 굳이 리렌더링이 될 필요가 없기 때문이다.
 
-	const onCreate = () => {
+	const onCreate = useCallback(() => {
 		const user = {
 			id: nextId.current,
 			username,
@@ -61,7 +76,21 @@ function App() {
 		});
 		console.log(nextId.current);
 		nextId.current += 1;	// 함수가 실행될 때 마다, 기존값의 1을 더해줌.
-	}
+	}, [username, email, users]);
+
+	// 배열에서 특정 아이템을 삭제할 때에는 불변성을 지켜가면서 업데이트를 해줘야 하는데, 이때는 filter 를 쓰는게 좋다.
+	const onRemove = useCallback(id => {
+		setUsers(users.filter(user => user.id !== id));
+	}, [users]);
+
+	const onToggle = useCallback(id => {
+		setUsers(users.map(
+			user => user.id === id
+			? {...user, active: !user.active} : user
+		));
+	}, [users]);
+
+	const count = useMemo(() => countActiveUsers(users), [users]);
 
 	return (
 		<>
@@ -76,13 +105,18 @@ function App() {
 			<InputSampleList/>
 			<UserList />
 			<UserListArray />
-			<UserListArrayUseRef users = {users}/>
+			<UserListArrayUseRef
+				users = {users}
+				onRemove={onRemove}
+				onToggle={onToggle}
+			/>
 			<CreateUser
 				username={username}
 				email={email}
 				onChange={onChange}
 				onCreate={onCreate}
 			/>
+			<div>활성 사용자 수: {count}</div>
 		</>
 	);
 }
--- begin-react/src/UserListArrayUseRef.js
+++ begin-react/src/UserListArrayUseRef.js
@@ -1,29 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function User({user}) {
+function User({user, onRemove, onToggle}) {
+    const {username, email, id, active} = user;
+    /*
+    useEffect(() => {
+        console.log("컴포넌트가 화면에 나타남.");
+        // props -> state
+        // REST API
+        // D3 Video.js
+        // setInterval, setTimeout
+        return () => {  // 클린어함수를 일종의 뒷정리 함수라고 생각하면 됨.
+            // clearInterval, clearTimeout
+            // 라이브러리 인스턴스 제거
+            console.log("컴포넌트가 화면에서 사라짐.");
+        }
+    }, [])
+     */
+
+    /*
+    useEffect(() => {
+        console.log("user 값이 설정됨.")
+        console.log(user);
+        return () => {
+            // 해당 함수가 변경 되기 전에는 clean-up 함수가 호출이 됨.
+            console.log("user 값이 바뀌기 전")
+            console.log(user);
+        }
+    }, [user]); // user 값이 설정되거나 바뀔때 마다 호출 (화면이 나타날때도 포함)
+     */
+
+    /*
+    useEffect(() => {
+        console.log(user);
+    });
+     */
+    // deps 를 넣지 않을 경우, 컴포넌트가 리렌더링 되고 나서 호출이 됨. 설정이 바뀔 때,
+    // 바뀌는 설정만 보여지는게 아니라 모든 컴포넌트에서 useEffect 가 실행됨.
+    // 이유는 리액트 컴포넌트에서는 부모컴포넌트가 리렌더링 되면
+    // 자식컴포넌트도 리렌더링 되기 때문에.
+
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b style={{
+                color: active ? 'green' : 'black',
+                cursor: 'pointer'
+            }}
+            onClick={()=>onToggle(id)}
+            >
+                {username}
+            </b>
+            &nbsp;
+            <span>({email})</span>
+            <button onClick={() => onRemove(id)}>삭제</button>
         </div>
     )
 }
 
-function UserListArrayUseRef({ users }) {
+function UserListArrayUseRef({ users, onRemove, onToggle}) {
     const style = {
         marginTop: '20px'
     };
 
     return (
         <div style={style}>
-            {users.map(
+            {
+                users.map(
                 /*
                  고유값이 없을 경우,
                  기본적으로 index 사용하게 되어있지만 단순히 index 를 넣었을 경우
                  성능적으로 좋아지거나 하지는 않다.
                  왠만해서는 피해주는게 좋다.
                  */
-                (user, index) => (<User user={user} key={user.id}/>)
-            )}
+                    (user, index) => (
+                        <User
+                            user={user}
+                            key={user.id}
+                            onRemove={onRemove}
+                            onToggle={onToggle}
+                        />
+                    )
+                )
+            }
         </div>
     );
 }
