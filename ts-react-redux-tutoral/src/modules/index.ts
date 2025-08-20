33d2df0133a79439848bb30e4b2b12fb5d8199bb
han-na-yang <hn.yang@apexesc.com>
2025-08-20 09:58:39 +0900

    [ts-react-redux-tutoral] 리덕스 사용 Counter (rootReducer)

---
+++ ts-react-redux-tutoral/src/modules/index.ts
@@ -0,0 +1,10 @@
+import {combineReducers} from "redux";
+import counter from "./counter";
+
+const rootReducer = combineReducers({
+    counter,
+})
+
+export default rootReducer;
+// 리덕스스토어에서 관리하고 상태에 대한 타입 생성
+export type RootState = ReturnType<typeof rootReducer>;
