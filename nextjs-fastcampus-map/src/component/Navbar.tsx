2611bdf432fb9d24b875fa9abb940725bb88e43a
yangrangrang <gkssk2309@gmail.com>
2025-08-26 20:24:57 +0900

    [nextjs-fastcampus-map] tailwind css
    v4 - v3과 다름 global.css 에서 보면 @layer로 감싸야함.

--- nextjs-fastcampus-map/src/component/Navbar.tsx
+++ nextjs-fastcampus-map/src/component/Navbar.tsx
@@ -10,18 +10,18 @@ export default function Navbar() {
     return (
         <>
             <div className="navbar">
-                <div className="navbar__logo">nextmap</div>
+                <Link href="/" className="navbar__logo">nextmap</Link>
                 <div className="navbar__list">
-                    <Link href="stores" className="navbar__list--item">
+                    <Link href="/stores" className="navbar__list--item">
                         맛집 목록
                     </Link>
-                    <Link href="stores/new" className="navbar__list--item">
+                    <Link href="/stores/new" className="navbar__list--item">
                         맛집 등록
                     </Link>
-                    <Link href="users/likes" className="navbar__list--item">
+                    <Link href="/users/likes" className="navbar__list--item">
                         찜한 가게
                     </Link>
-                    <Link href="users/login" className="navbar__list--item">
+                    <Link href="/users/login" className="navbar__list--item">
                         로그인
                     </Link>
                 </div>
@@ -37,16 +37,16 @@ export default function Navbar() {
             {isOpen && (
                 <div className="navbar--mobile">
                     <div className="navbar__list--mobile">
-                        <Link href="stores" className="navbar__list--item--mobile">
+                        <Link href="/stores" className="navbar__list--item--mobile">
                             맛집 목록
                         </Link>
-                        <Link href="stores/new" className="navbar__list--item--mobile">
+                        <Link href="/stores/new" className="navbar__list--item--mobile">
                             맛집 등록
                         </Link>
-                        <Link href="users/likes" className="navbar__list--item--mobile">
+                        <Link href="/users/likes" className="navbar__list--item--mobile">
                             찜한 가게
                         </Link>
-                        <Link href="users/login" className="navbar__list--item--mobile">
+                        <Link href="/users/login" className="navbar__list--item--mobile">
                             로그인
                         </Link>
                     </div>
