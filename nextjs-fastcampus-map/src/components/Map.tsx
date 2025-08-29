f13239f9c75925e2ba7ea2ceb1d9d57369fe2fca
yangrangrang <gkssk2309@gmail.com>
2025-08-29 19:57:12 +0900

    [nextjs-fastcampus-map] kakao marker

--- nextjs-fastcampus-map/src/components/Map.tsx
+++ nextjs-fastcampus-map/src/components/Map.tsx
@@ -1,12 +1,16 @@
 /*global kakao*/
 
 import Script from "next/script";
+import * as stores from "@/data/store_data.json";
 
 declare global {
     interface Window {
         kakao:any;
     }
 }
+
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
 
 export default function Map() {
     console.log("test");
@@ -19,10 +23,26 @@ export default function Map() {
         window.kakao.maps.load(() => {
             const mapContainer = document.getElementById("map");
             const mapOption = {
-                center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+                center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
                 level: 3,
             };
-            new window.kakao.maps.Map(mapContainer, mapOption);
+            const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+            // 식당 데이터 마커 띄우기
+            stores?.["DATA"]?.map((store) => {
+                // 마커가 표시될 위치
+                const markerPosition = new window.kakao.maps.LatLng(
+                    store?.y_dnts,
+                    store?.x_cnts,
+                );
+
+                // 마커 생성
+                const marker = new window.kakao.maps.Marker({
+                    position: markerPosition,
+                });
+
+                marker.setMap(map);
+            });
         })
     }
     return (
